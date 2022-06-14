3d73660f10bbfb3b2eb43e915f6ba58b2121baac
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-06-13 21:14:09 -0300

    changed time

--- twitterClient.js
+++ twitterClient.js
@@ -3,10 +3,10 @@ const { TwitterApi } = require("twitter-api-v2");
 require("dotenv").config()
 
 const client = new TwitterApi({
-    appKey: process.env.appKey,
-    appSecret: process.env.appSecret,
-    accessToken: process.env.accessToken,
-    accessSecret: process.env.accessSecret
+    appKey:         process.env.appKey,
+    appSecret:      process.env.appSecret,
+    accessToken:    process.env.accessToken,
+    accessSecret:   process.env.accessSecret
 })
 
 const rwClient = client.readWrite
